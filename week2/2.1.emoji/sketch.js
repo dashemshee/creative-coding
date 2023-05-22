67d78c2a13836fc182b78bde7f76268091e4aa49
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-05-22 15:18:41 -0400

    sketch

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,33 @@ function setup() {
 
 function draw() {
   background(220);
+  fill("yellow");
+  ellipse(200,200,250,250);
+
+  fill("pink");
+ellipse(275,225,40,40);
+ellipse(118,225,40,40);
+
+
+  fill("red");
+  ellipse(150,150,70,90);
+  ellipse(250,150,70,90);
+
+  stroke("black");
+  fill("white");
+  ellipse(150,150,40,50);
+  ellipse(250,150,40,50);
+
+  fill("black");
+  square(150, 150, 20, 20);
+  square(250,150,20,20);
+describe(
+  'white square with black outline and round edges in mid-right of canvas'
+);
+
+ellipse(200,260,100,100);
+
+
+
+
 }
