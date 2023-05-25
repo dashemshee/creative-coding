8f44aa2ee96bd3207c8b0a9b512b24b97f8b9e59
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-05-24 21:17:58 -0400

    update

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,48 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
+  noLoop();
 }
-
 function draw() {
   background(220);
+  angleMode(DEGREES);
+
+    //quilt colors
+  const colors = ['pink', 'lightblue', 'black'];
+  const letters = ['A', 'B', 'C'];
+
+
+  // quilt pattern
+  const pattern = [
+    ['A', 'B', 'C', 'A', 'C', 'B', 'A'],
+    ['C', 'A', 'B', 'C', 'B', 'A', 'C'],
+    ['B', 'C', 'A', 'B', 'A', 'C', 'B'],
+    ['A', 'B', 'C', 'A', 'C', 'B', 'A'],
+    ['B', 'C', 'A', 'B', 'A', 'C', 'B'],
+    ['C', 'A', 'B', 'C', 'B', 'A', 'C'],
+    ['A', 'B', 'C', 'A', 'C', 'B', 'A']
+       ];
+
+  const numRows = pattern.length;
+  const numCols = pattern[0].length;
+
+  //size
+  const blockWidth = width/numCols;
+  const blockHeight = height/numRows;
+
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      const letter = pattern[row][col];
+
+      const index = letters.indexOf(letter);
+      const color = colors[index];
+
+      push();
+
+      translate(col * blockWidth, row * blockHeight);
+      fill(color);
+      rect(0, 0, blockWidth, blockHeight);
+
+      pop();
+    }
+     }
 }
