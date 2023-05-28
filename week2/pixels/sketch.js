e8c9987427026bef1cace80c872bad80086b48c1
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-05-28 18:56:22 -0400

    image

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(800, 800);
+let img;
+
+
+function preload(){
+
+
 }
 
+
+function setup() {
+  createCanvas(800, 800);
+ }
+
 function draw() {
-  background(220);
+background(220);
+for (let x = 0; x < img.width; x += 10) {
+  for (let y = 0; y < img.height; y += 10) {
+    const c = getPixelColor(x, y, c);
+  }
+}
 }
