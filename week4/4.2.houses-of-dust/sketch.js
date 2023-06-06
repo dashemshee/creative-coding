043b5a9364666ffb28c566813b7031bd8c4bb949
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-06-06 17:23:25 -0400

    update

--- week4/4.2.houses-of-dust/sketch.js
+++ week4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,42 @@
-function setup() {
-  createCanvas(800, 800);
+let backgroundImage;
+let poem = '';
+
+let grammar = tracery.createGrammar({
+  "origin": ["A HOUSE OF #material#, #place#, USING #light_source#, INHABITED BY #inhabitants#"],
+  "material": ["DUST", "BRICK", "PAPER", "LEAVES", "ROOTS", "DISCARDED CLOTHING", "WEEDS", "WOOD", "TIN", "SAND", "GLASS", "STONE", "STRAW", "PLASTIC", "MUD", "BROKEN DISHES", "PLASTIC"],
+  "place": ["CITY", "FOREST", "DESERT", "OCEAN", "MOUNTAIN", "GARDEN", "CAVE", "SPACE", "SKY"],
+  "light_source": ["SUNLIGHT", "CANDLELIGHT", "MOONLIGHT", "STARLIGHT", "LAMP", "FIRELIGHT", "ELECTRIC LIGHT"],
+  "inhabitants": ["PEOPLE", "ANIMALS", "PLANTS", "SPIRITS", "ROBOTS", "GHOSTS"] });
+
+function preLoad() {
+  backgroundImage = loadImage('')
 }
+
+function setup()  {
+  createCanvas(800, 800);
+  frameRate(.2);
+  textAlign(CENTER, CENTER);
+  }
 
 function draw() {
   background(220);
+  poem = grammar.flatten("#origin#");
+  textFont("bold");
+
+  let fontSize = width / 15;
+  textSize(fontSize);
+
+  let poemWidth = textWidth(poem);
+  while (poemWidth > width - 100) {
+    fontSize -= 1;
+    textSize(fontSize);
+    poemWidth = textWidth(poem);
+  }
+
+let lineHeight =  fontSize * 1.2;
+textLeading(lineHeight);
+
+
+fill('red');
+text(poem, width / 2, height / 2);
 }
