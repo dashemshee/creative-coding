e151236e3591b4ca1f8145e966fa9cf381fa3ac2
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-06-09 13:49:38 -0400

    update

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,35 @@
-function setup() {
-  createCanvas(800, 800);
-}
 
-function draw() {
-  background(220);
-}
+//markov model
+let rm = RiTa.markov(3);
+
+// finding data
+let data = document.getElementById("source").innerText;
+
+
+//train model
+rm.addText(data);
+
+
+function setup() {
+ 
+ let lines = rm.generate(6);
+
+
+ //find letter div
+ let letter = select("#letter");
+
+
+//salutation
+ letter.child( createP("Dear Catherine,") );
+
+ for (let l = 0; l < lines.length; l++ ){
+    letter.child(
+        createP( lines[l]  )
+
+      );
+
+ }
+
+//closing
+ letter.child( createP("Yours Truly,<br> Mr Gatsby")  );
+ }
