17a5d2687a3381e8eb58c8171af13b10824e4c74
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-06-15 14:49:44 -0400

    update

--- week3/3.4.screen-saver/sketch.js
+++ week3/3.4.screen-saver/sketch.js
@@ -1,19 +1,30 @@
-let earthAni;
-let earthX = 0;
+let backgroundImages = [];
+let currentBackgroundIndex = 0;
+let interval = 3000;
+let backgroundImage;
+
+function preload() {
+  backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/earth3.jpg'));
+  backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/earth4.jpg'));
+  backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/earth5.jpg'));
+}
 
 function setup() {
-  createCanvas(500, 160);
-  earthAni = loadAnimation('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/earth3.jpg');
+  createCanvas(1000, 600);
+  setBackgroundImage(currentBackgroundIndex);
+  setInterval(changeBackground, interval);
 }
 
 function draw() {
   background(0);
-  earthX += 1; 
+  image(backgroundImage, 0, 0, width, height);
+}
 
-  dge
-  if (earthX > width + earthAni.width) {
-    earthX = -earthAni.width;
-  }
+function setBackgroundImage(index) {
+  backgroundImage = backgroundImages[index];
+}
 
-  animation(earthAni, earthX, 80);
+function changeBackground() {
+  currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
+  setBackgroundImage(currentBackgroundIndex);
 }
