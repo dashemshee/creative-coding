0d2dccdd2fb67c6bf2753514417a9318d09d6f51
dashemshee <133787968+dashemshee@users.noreply.github.com>
2023-06-16 09:45:02 -0400

    update

--- week3/3.4.screen-saver/sketch.js
+++ week3/3.4.screen-saver/sketch.js
@@ -1,11 +1,9 @@
 let backgroundImages = [];
 let currentBackgroundIndex = 0;
 
-//image will change every 
+//interval 
 let interval = 6000;
 let backgroundImage;
-
-//adding ripple effect tomorrow//
 
 
 //images came from google (earthcam) website//
@@ -13,7 +11,7 @@ function preload() {
   backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/geo1.jpg'));
   backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/geo3.jpg'));
   backgroundImages.push(loadImage('https://dashemshee.github.io/creative-coding/week3/3.4.screen-saver/earth3.jpg'));
-}
+  }
 
 function setup() {
   createCanvas(1000, 600);
@@ -30,24 +28,25 @@ function draw() {
   fill('white');
   textAlign(CENTER, CENTER);
   text(formatTime(now), width / 2, height / 2);
-}
+  }
 
 function setBackgroundImage(index) {
   backgroundImage = backgroundImages[index];
-}
+  }
 
 function changeBackground() {
   currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
   setBackgroundImage(currentBackgroundIndex);
-}
-
+  }
+//time
 function formatTime(date) {
   let hours = formatDigits(date.getHours());
   let minutes = formatDigits(date.getMinutes());
   let seconds = formatDigits(date.getSeconds());
   return hours + ":" + minutes + ":" + seconds;
-}
+
+  } 
 
 function formatDigits(value) {
   return value < 10 ? "0" + value : value;
-}
+  }
